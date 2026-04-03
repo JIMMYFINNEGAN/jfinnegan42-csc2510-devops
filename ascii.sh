96ba5de98d54d03b38a100499e4aeb49e2d761e6
Jimmy Finnegan <jfinnegan42@tntech.edu>
2026-04-03 16:49:22 +0000

    Add ascii art

---
+++ ascii.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo -e "\e[34m"
+echo "        ^"
+echo "       / \\"
+echo "      / _ \\"
+echo "     |.o '.|"
+echo "     |'._.'|"
+echo "     |     |"
+echo "    /|     |\\"
+echo "   /_|_____|_\\"
+echo "     /_/ \\_\\"
+echo -e "\e[0m"
